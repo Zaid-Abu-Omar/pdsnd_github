3833e8aa9c5c9f3213f40f705b442b0ec881beda
Zaid <zaidabuomar60@gmail.com>
2025-11-24 23:33:51 +0200

    Add commits to bikeshare.py functions

---
+++ bikeshare.py
@@ -0,0 +1,9 @@
+
+def total_trip_duration(data):
+        """
+        Calculates the total duration of all trips in the dataset. 
+           
+        Here is a simple code to calculate the total of trips durations
+        """
+        total_duration = sum(data)
+        return total_duration
